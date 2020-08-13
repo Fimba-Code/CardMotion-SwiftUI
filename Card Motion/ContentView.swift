8e1340b05c984c58898d43a5fae048bc73b7dc90
Mr. Robot <joselsondias@hotmail.com>
2020-08-13 20:19:29 +0100

    Initial Commit

---
+++ Card Motion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Card Motion
+//
+//  Created by MR.Robot 💀 on 13/08/2020.
+//  Copyright © 2020 Joselson Dias. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
